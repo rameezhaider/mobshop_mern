ed4e80b3a87c5151f88a0a24f454552ba4491c1b
Syed Rameez Haider <rameeznaqvi9@gmail.com>
2021-06-26 06:42:10 -0700

    Add build

---
+++ frontend/build/precache-manifest.cc74aca3fab8dea475ae83b71563716a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a6f1fc7adc95005faa150fd3612a8fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f613b74da12ddfe48021",
+    "url": "/static/css/main.1c6d6e73.chunk.css"
+  },
+  {
+    "revision": "4ea7726c52bc4613c6ca",
+    "url": "/static/js/2.7303df09.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/static/js/2.7303df09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f613b74da12ddfe48021",
+    "url": "/static/js/main.e0e6a8f5.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
